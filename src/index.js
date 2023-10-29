9fd156ec0964d15cd824f0a365f0085cc3ff7d6a
01686611529a <nguyenngoclongpdl13@gmail.com>
2023-10-29 22:44:36 +0700

    long

--- src/index.js
+++ src/index.js
@@ -215,5 +215,5 @@ app.post("/send", upload.none(), function (req, res) {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
